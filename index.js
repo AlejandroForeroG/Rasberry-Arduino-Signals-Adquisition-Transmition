a10031026c7fa74a8b1e43336372b38f1121bedf
AlejandroForeroG <alejandro.forerog@gmail.com>
2023-03-05 21:06:13 +0000

    In this commit we are able to adquire the data from arduino with serialPort Module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {SerialPort} =require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline')
 
 console.log("Importado");
 
@@ -6,5 +7,12 @@ const port = new SerialPort(
     {path:'/dev/ttyACM0',
     baudRate:115200
 });
-// const Readline = SerialPort.parsers.Readline();
-// console.log(Readline)
+
+
+
+port.on("open", () => {
+    console.log('serial port open');
+});
+port.on('data', (data) => {
+    console.log('got word from arduino:', data.toString());
+});
