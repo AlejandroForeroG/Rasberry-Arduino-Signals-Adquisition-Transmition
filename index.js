97b898bd29b05c549582b4d4acecbfc542d6e209
AlejandroForeroG <alejandro.forerog@gmail.com>
2023-04-05 00:06:33 +0100

    In this commit i added  the event listener for a button which is clicked in server side, and this button initialize the transmision of the data

--- index.js
+++ index.js
@@ -1,56 +1,71 @@
 const {SerialPort} =require('serialport');
 const {ReadlineParser } = require('@serialport/parser-readline')
 const io = require('socket.io-client')
-const { getTime,getDate} =  require('./obj');
-
-
+const { getTime,getDate} =  require('./time');
 console.log("Importado");
+
 
 const port = new SerialPort(
     {path:'/dev/ttyACM0',
     baudRate:115200
 });
-
+//socket establishment and serial port 
 const parser = port.pipe(new ReadlineParser("\n"));
-
-//socket establishment
 const socket = io.connect('http://192.168.10.20:3000');
+
+
+
+var prober = 0;
+var samples = 0;
 
 //scoket events 
 socket.on('connect',()=>{
-    console.log("Coneccion establecida con el servidor");
 
-    parser.on('open', () => {
-        console.log("coneccion abierta");
-    });
 
-    parser.on('data', (data) => {
-        //processing data 
-        let splitter =data.split(" ");
-
-        //transmission 
-        socket.emit('rasberry:data', {
-            sampleGen:splitter[0],
-            date: getDate("/"),
-            time: getTime(),
-            temperature: splitter[1],
-            bmp:splitter[2],
-            oxigenSaturation:splitter[3],
-            gsrResistance:splitter[4],
-            grsVoltage:splitter[5],
-            airflux: splitter[6]
+        console.log("Coneccion establecida con el servidor");
+        console.log("inicio de transmision")
+        
+        parser.on('open', () => {
+            console.log("coneccion abierta");
         });
 
-    })
+        parser.on('data', (data) => {
+            if(prober==1){
+            //processing data 
+            samples++
+            let splitter =data.split(" "); 
+            console.log(splitter[0])
+            socket.emit('rasberry:data', {
+                sample:samples,
+                date: getDate("/"),
+                time: getTime(),
+                temperature: splitter[1],
+                bpm :splitter[2],
+                oxigenSaturation:splitter[3],
+                gsrResistance:splitter[4],
+                grsVoltage:splitter[5],
+                airflux: splitter[6]
+            });
+        }else{
+            samples = 0;
+        }
+        })
+    
+    
 
     socket.on("disconnect",()=>{
         console.log("desconeccion del servidor");
     })
 
 
-})
+    socket.on('btninit',(state)=>{
+      prober=state;
+ 
+    })
 
-//functions
+
+
+})
 
 
 
